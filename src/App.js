1584c02e4de9dea57777fe26c37cc9315f17af7e
andja <andja.velimirovic@gmail.com>
2022-09-30 23:00:15 +0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.scss";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import WorkExperience from "./components/WorkExperience";
+import Intro from "./components/Intro";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App" id="home">
+      <Header />
+      <Intro />
+      <AboutMe />
+      <Projects />
+      <WorkExperience />
+      <Contact />
+    </main>
   );
 }
 
