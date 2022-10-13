040351a0bd5d75c49e03040e66fada321b760c58
andja <andja.velimirovic@gmail.com>
2022-10-13 22:30:27 +0200

    side email

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -4,37 +4,46 @@ import style from "./SideBar.module.scss";
 
 export default function SideBar() {
   return (
-    <div className={style.icons}>
-      <ul className={style.list}>
-        <li>
-          <a
-            href={"https://github.com/andjared"}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GitHub size={25} />
+    <>
+      <div className={`${style.sidebar} ${style.left}`}>
+        <ul className={` ${style.wrapper} ${style.list}`}>
+          <li>
+            <a
+              href={"https://github.com/andjared"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitHub size={25} />
+            </a>
+          </li>
+          <li>
+            <a
+              href={"https://www.linkedin.com/in/andja-zelenbaba-464673134/"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Linkedin size={25} />
+            </a>
+          </li>
+          <li>
+            <a href={"mailto:zelenbaba.andja@gmail.com"}>
+              <Mail size={25} />
+            </a>
+          </li>
+          <li>
+            <a href={"tel:+381652040432"}>
+              <Phone size={25} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className={`${style.sidebar} ${style.right}`}>
+        <div className={style.wrapper}>
+          <a href="mailto:zelenbaba.andja@gmail.com">
+            zelenbaba.andja@gmail.com
           </a>
-        </li>
-        <li>
-          <a
-            href={"https://www.linkedin.com/in/andja-zelenbaba-464673134/"}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Linkedin size={25} />
-          </a>
-        </li>
-        <li>
-          <a href={"mailto:zelenbaba.andja@gmail.com"}>
-            <Mail size={25} />
-          </a>
-        </li>
-        <li>
-          <a href={"tel:+381652040432"}>
-            <Phone size={25} />
-          </a>
-        </li>
-      </ul>
-    </div>
+        </div>
+      </div>
+    </>
   );
 }
