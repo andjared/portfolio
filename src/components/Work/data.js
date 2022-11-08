a53500bef3b5bd35837293fc5e8941eaeda8fea4
andja <andja.velimirovic@gmail.com>
2022-11-08 11:43:58 +0100

    jobs file, fixed projects links

---
+++ src/components/Work/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    department: "Department for missing persons",
+    role: "Police Inspector",
+    company: "Ministry of Interior",
+    range: "December 2017 - May 2022",
+    id: 1,
+    duties: [
+      `Administrative, operational and field activities related to missing persons`,
+      `Department management in the absence of the Chief of the Department`,
+      `Producing daily and monthly reports`,
+    ],
+  },
+  {
+    department: "Administration",
+    role: "Office Administrator",
+    company: "Driving School Vidana",
+    range: "October 2016 - June 2017",
+    id: 2,
+    duties: [
+      `Client communication, business correspondence`,
+      `Contracts and billing`,
+      `Office supplies procurement`,
+    ],
+  },
+];
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,75 +1,52 @@
 import React from "react";
 import { useState } from "react";
 import styles from "./Work.module.scss";
+import { data } from "./data.js";
 
 export default function Work() {
-  const [isActive, setIsActive] = useState("first");
+  const [isActive, setIsActive] = useState(1);
 
   return (
     <section id="work-experience" className={styles.work}>
       <h2 className="heading">Work experience</h2>
       <div className={styles.inner}>
         <div className={styles.jobs}>
-          <button
-            className={
-              isActive === "first"
-                ? `${styles.btn} ${styles.active}`
-                : styles.btn
-            }
-            onClick={() => setIsActive("first")}
-          >
-            <span>Department for missing persons</span>
-          </button>
-          <button
-            className={
-              isActive === "second"
-                ? `${styles.btn} ${styles.active}`
-                : styles.btn
-            }
-            onClick={() => setIsActive("second")}
-          >
-            <span>Driving school Vidana</span>
-          </button>
+          {data.map((job) => {
+            return (
+              <button
+                key={job.department}
+                className={
+                  isActive === job.id
+                    ? `${styles.btn} ${styles.active}`
+                    : styles.btn
+                }
+                onClick={() => setIsActive(job.id)}
+              >
+                <span>{job.department}</span>
+              </button>
+            );
+          })}
         </div>
-        <div
-          className={isActive === "first" ? styles.description : styles.hidden}
-        >
-          <h3>
-            <span className={styles.role}>Police Inspector</span>
-            <span className={styles.company}>
-              &nbsp;@&nbsp; Ministry of Interior
-            </span>
-          </h3>
-          <p className={styles.range}>December 2017 - May 2022</p>
-          <ul>
-            <li>
-              Administrative, operational and field activities related to
-              missing persons
-            </li>
-            <li>
-              Department management in the absence of the Chief of the
-              Department
-            </li>
-            <li>Producing daily and monthly reports</li>
-          </ul>
-        </div>
-        <div
-          id="second"
-          className={isActive === "second" ? styles.description : styles.hidden}
-        >
-          <h3>
-            <span className={styles.role}>Office Administrator</span>
-            <span className={styles.company}>
-              &nbsp;@&nbsp; Driving school Vidana
-            </span>
-          </h3>
-          <p className={styles.range}>October 2016 - June 2017</p>
-          <ul>
-            <li>Client communication, business correspondence</li>
-            <li>Contracts and billing</li>
-            <li>Office supplies procurement</li>
-          </ul>
-        </div>
+        {data.map((job) => {
+          const { role, company, range, id, duties } = job;
+          return (
+            <div
+              key={id}
+              className={isActive === id ? styles.description : styles.hidden}
+            >
+              <h3>
+                <span className={styles.role}>{role}</span>
+                <span className={styles.company}>&nbsp;@&nbsp; {company}</span>
+              </h3>
+              <p className={styles.range}>{range}</p>
+              <ul>
+                {duties.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
