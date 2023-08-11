5fb59e557dcdf75a867332ca216877ab45b36832
andja <andja.velimirovic@gmail.com>
2023-08-11 15:05:34 +0200

    add work

--- src/components/Work/data.js
+++ src/components/Work/data.js
@@ -1,10 +1,21 @@
 export const data = [
   {
+    department: "Front End Development",
+    role: "Mentorship Program",
+    company: "Maxwell Engineering",
+    range: "February 2023 - May 2023",
+    id: 1,
+    duties: [
+      `Worked under supervision of a senior developer. During mentorship I’ve built several projects which helped me to improve my skills and deepen my understanding in the area of front end development. 
+  `,
+    ],
+  },
+  {
     department: "Department for missing persons",
     role: "Police Inspector",
     company: "Ministry of Interior",
     range: "December 2017 - May 2022",
-    id: 1,
+    id: 2,
     duties: [
       `Administrative, operational and field activities related to missing persons`,
       `Department management in the absence of the Chief of the Department`,
@@ -16,7 +27,7 @@ export const data = [
     role: "Office Administrator",
     company: "Driving School Vidana",
     range: "October 2016 - June 2017",
-    id: 2,
+    id: 3,
     duties: [
       `Client communication, business correspondence`,
       `Contracts and billing`,
